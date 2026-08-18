c52841e803c04d3c4532243e3397e11645dced89
Matt Callaghan <mcallaghan@naperville203.org>
2026-08-18 14:56:40 -0500

    Wrote ch 1 notes through identifiers

---
+++ Chapter1.java
@@ -0,0 +1,27 @@
+/*
+ * Every Java program contains one or more classes (e.g., Chapter1).
+ * 
+ * In general, every source file (e.g., Chapter1.java) contains one class (e.g., Chapter1).
+ * 
+ * The source file name and the class name MUST match!
+ * 
+ * By convention, class names start with an uppercase letter.
+ */
+
+public class Chapter1
+{
+    /*
+     * A class contains methods.
+     * 
+     * Most Java programs contain a class with a main method.
+     * 
+     * The main method is executed when the program starts.
+     */
+    public static void main(String[] args)
+    {
+        /*
+         * A method contains statements
+         */
+        System.out.println("Hello, world.");
+    }
+}
