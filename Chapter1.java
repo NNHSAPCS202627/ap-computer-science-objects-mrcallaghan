d6bab7f2aa7f30d7feec45f145bf17df2e34197f
Matt Callaghan <mcallaghan@naperville203.org>
2026-08-20 08:27:59 -0500

    Finished Ch. 1 notes

--- Chapter1.java
+++ Chapter1.java
@@ -11,13 +11,13 @@
 
 public class Chapter1
 {
-   /*
-    * A class contains methods.
-    * 
-    * Most Java programs contain a class mith a main method.
-    * 
-    * The main method is executed when the program starts.
-    */
+    /*
+     * A class contains methods.
+     * 
+     * Most Java programs contain a class mith a main method.
+     * 
+     * The main method is executed when the program starts.
+     */
     public static void main(String[] args)
     {
         /*
@@ -30,7 +30,7 @@ public class Chapter1
          * When invoking a method, arguments are passed in parentheses (e.g., "Hello, world.")
          */
         System.out.println("Hello, world.");
-        
+
         /*
          * A statement contains identifiers.
          * 
@@ -43,34 +43,61 @@ public class Chapter1
          * These are valid identifiers:
          */
         int y, x2, X_Y, total$;
-        
+
         // this is a single-line comment (like # in python)
         // this is not a valid identifier
         //int 2y;
-        
+
         /*
          * Identifeirs are case sensitive (case matters)
          * 
          * These are different:
          */
         String squirt, SQuiRT, SQUIRT;
-        
+
         /*
          * By convention, constants are all uppercase with an underscore to separate words.
          * 
          */
         int SPEED_OF_LIGHT = 300000000; // m/s
+
+        /*
+         * By convention, most other identifiers start with a lowercase letter.
+         *      Subsequent words start with an uppercase letter (Camalcase)
+         */
+        String pacificOcean;
         
+        /*
+         * This is a compile-time error.  (The code doesn't compile)
+         * 
+         * These are often called syntax errors.
+         */
+        //System.out.pritln("Hello, world.");
         
+        /*
+         * This is a run-time error.  (The code compiles and runs,
+         *  but generates an exception.  In most cases the program crashes.
+         */
         
+        //System.out.println(1/0);
+        
+        /*
+         * This is also a run-time error.  (The code compiles and runs, but doesn't produce the expected output)
+         * 
+         * More specifically, this is a logical error as the output differs from my intention.
+         */
+        System.out.println("Hello, word.");
+        
+        /*
+         * Algorithm - aseries of steps we can follow.  Must be:
+         *  - unambiguous
+         *  - executable
+         *  - terminating  
+         *  
+         */
+
     }
 }
-
-
-
-
-
-
 
 
 
