c7e4cb5d11f1dd32e8f93d6d8653d279a15b84e3
Matt Callaghan <mcallaghan@naperville203.org>
2026-08-20 14:41:17 -0500

    Began ch1 notes.

--- Chapter1.java
+++ Chapter1.java
@@ -1,9 +1,33 @@
-
-
+/*
+ * Every Java program has at least on class (e.g., Chapter1).
+ * 
+ * In general, every source file (e.g., Chapter.java) contains one class (e.g., Chapter1).
+ * 
+ * The source file name must match the class name.
+ * 
+ * By convention, all calss names start with a uppercase letter.
+ */
 public class Chapter1
 {
+    /*
+     * A call contains methods.
+     * 
+     * Most Java programs will have a main method (see below).
+     * 
+     * In general, methods are invoked on objects.
+     * 
+     * WHen invoking a method, arguments are passed in parentheses (e.g., "Hello, world.")
+     */
     public static void main(String[] args)
     {
+        /*
+         * A method contains statments.
+         * 
+         * Statments may invoke other methods (e.g., println).
+         * 
+         * In Java, a stament ends with a semicolon.
+         */
         System.out.println("Hello, world.");
     }
 }
+
