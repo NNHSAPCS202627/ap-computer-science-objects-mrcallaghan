09f9297fb355966cd7c908e6b5d3044e2df30595
Matt Callaghan <mcallaghan@naperville203.org>
2026-08-24 14:01:48 -0500

    Started notes on Github terminology

--- Github.java
+++ Github.java
@@ -1,47 +1,19 @@
 
 /**
- * Notes on git/github terminology.
+ * Notes on Git/Github terminology and workflow
  *
  * @author mrcallaghan
  * @version 24aug2026
  */
 public class Github
 {
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public static void sampleMethod()
-    {
-        System.out.println("Hello, world.");
-    }
-    
     /*
-     * Git/Github Terminology
+     * Git - The software/tool that allows us to track changes in our code over time.
      * 
-     * Git - The software/tool that track changes in your code.
-     * 
-     * Github - An online website that can stro your Git repositories in the cloud.
-     * 
-     * Repository (repo) - A folder that Git is tracking.  It contains all of your projects files and a history of their changes.
-     * 
-     * Commit - A permanent snapshot of your project at a point in time.   Each commit message describes the changes made.
-     * 
-     * Push - Upload your commits to GitHUb so they saved online.
+     * Github - An online website that can store your Git repositories in the cloud.  A "backup" of you code
+     *          you always have access to on any device.
+     *          
+     * Repository (repo) - A folder (of code) that Git is tracking.  It contains all of your project files and
+     *          the entire history of changes.
      */
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
 }
