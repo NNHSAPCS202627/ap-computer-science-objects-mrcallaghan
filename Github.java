27f61594d57736ffcc337080c5bda820e1e976d8
Matt Callaghan <mcallaghan@naperville203.org>
2026-08-24 15:07:56 -0500

    Finshed git notes

--- Github.java
+++ Github.java
@@ -18,6 +18,13 @@ public class Github
      * Clone - Make a full copy of a Github repository onto your computer, including all the files and history. In BlueJ,
      *          the term is "checkout project".
      *          
+     * Commit - A permanent snapshot of your project at a point in time.  Includes a helpful message.
+     * 
+     * Commit message - A short description of the changes you write when you make a commit.
+     *          Make them descriptive, but not overly wordy.
+     *          
+     * Push - Upload your recent commits to GitHub so they are saved in the cloud.
+     *          
      * 
      */
 }
