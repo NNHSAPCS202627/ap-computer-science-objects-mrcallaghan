f92b6504840d83bf7b80dc667dc3e2abc902bc24
Matt Callaghan <mcallaghan@naperville203.org>
2026-08-24 08:39:52 -0500

    Added notes on git terminology

---
+++ Github.java
@@ -0,0 +1,20 @@
+
+/**
+ * Notes on git/github terminology.
+ *
+ * @author mrcallaghan
+ * @version 24aug2026
+ */
+public class Github
+{
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public static void sampleMethod()
+    {
+        System.out.println("Hello, world.");
+    }
+}
