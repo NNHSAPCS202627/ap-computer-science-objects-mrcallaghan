9699c2150b448387d6f245aa0c32802abbba797f
Matt Callaghan <mcallaghan@naperville203.org>
2026-08-25 07:57:44 -0500

    finsihed git notes

--- Github.java
+++ Github.java
@@ -7,14 +7,41 @@
  */
 public class Github
 {
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
+       
     public static void sampleMethod()
     {
         System.out.println("Hello, world.");
     }
+    
+    /*
+     * Git
+            The software/tool that tracks changes in your code.
+
+        GitHub
+            An online website that can store your Git repositories in the cloud. For solo use, 
+            it’s like having a “backup” of your repo that you can push to and pull from.
+
+        Repository (repo)
+            A folder that Git is tracking. It contains all your project files and the entire history of changes.
+
+        Commit
+            A permanent snapshot of your project at a point in time. Each commit has a message describing the change. 
+            In BlueJ, this is the "commit/push" button (i.e., commit the changes and push to the remote repository). Analogy: a save point in a video game.
+        
+        Commit Message
+            A short description of the changes you write when you make a commit.
+            
+        Push
+            Uploads your commits to GitHub so they’re saved online.
+        
+        Pull
+            Downloads the latest version of your repo from GitHub to your computer. 
+            Useful when working on multiple devices to update a local repository that is out of date. In BlueJ, 
+            this is what the "update" button does.
+            
+        Clone
+            Makes a full copy of a GitHub repository onto your computer, including all history and files. 
+            In BlueJ, this is what happens when you "checkout project"
+        
+     */
 }
