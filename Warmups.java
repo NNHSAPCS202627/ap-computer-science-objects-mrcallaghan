d9e898f14abc09987282e2709e420878c47835af
Matt Callaghan <mcallaghan@naperville203.org>
2026-08-27 07:55:46 -0500

    Wrote warmup #1

---
+++ Warmups.java
@@ -0,0 +1,15 @@
+/*
+ * Unit 1 warm-ups
+ */
+public class Warmups
+{
+    public static void warmup1()
+    {
+        //write solution here
+        World myWorld = new World();
+        Turtle pumpkin = new Turtle(myWorld);
+        pumpkin.forward(25);
+
+
+    }
+}
