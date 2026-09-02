26ba247ee08dc0d4cb8d2ea720c0bf8848275ba9
mrcallaghan <mcallaghan@naperville203.org>
2026-09-02 14:39:47 -0500

    solved warmup #2

---
+++ Warmups.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class Warmups here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Warmups
+{
+    public static void warmup1()
+    {
+
+    }
+
+    public static void warmup2()
+    {
+        String city = new String("Naperville");
+        //String city = “Naperville”;  // literal
+        String bigCity = city.toUpperCase();
+        System.out.println(city + " / " + bigCity);
+
+    }
+}
